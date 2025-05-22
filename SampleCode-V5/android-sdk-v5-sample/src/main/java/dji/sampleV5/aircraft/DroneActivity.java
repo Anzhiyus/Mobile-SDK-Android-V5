c31de7b7aa18e294bf7b3f1182f56804a5c31618
Anzhiyus <1666103438@qq.com>
2025-05-22 17:00:44 +0800

    Modify：将航线飞行界面设置为初始界面。

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/DroneActivity.java
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/DroneActivity.java
@@ -1,92 +1,23 @@
 package dji.sampleV5.aircraft;
 
-import static dji.sampleV5.aircraft.DroneActivity0.getMatchingFileNames;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.work.Data;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkInfo;
-import androidx.work.WorkManager;
-import androidx.work.WorkRequest;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.DMatch;
-import org.opencv.core.KeyPoint;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfDMatch;
-import org.opencv.core.MatOfKeyPoint;
-import org.opencv.core.Point;
-import org.opencv.features2d.DescriptorMatcher;
-import org.opencv.features2d.ORB;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 //import dji.sampleV5.aircraft.pages.DroneFlyFragment;
-import dji.sampleV5.aircraft.pages.MediaFragment;
+import dji.sampleV5.aircraft.pages.WayPointV3Fragment;
 
 public class DroneActivity extends AppCompatActivity {
 
-    private ImageView imageView; // 显示图像的ImageView
-    private String[] pictureArray;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drone);
 
-//        // 动态加载 FlyFragment
-//        if (savedInstanceState == null) {  // 确保 Fragment 只会添加一次
-//            getSupportFragmentManager().beginTransaction()
-//                    .replace(R.id.fragment_container, new DroneFlyFragment()) // 假设有一个容器 view 来放置 Fragment
-//                    .commit();
-//        }
-
-        imageView = findViewById(R.id.imageView);
-
-        // 从Intent中获取图像文件路径
-
-        pictureArray = getMatchingFileNames(
-                getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString() + "/ProcessedImages",
-                "^Picture_.*\\.(jpg|JPG)"
-        );
-        String filePath = pictureArray[0];
-
-        if (filePath != null) {
-            // 加载图片
-            Bitmap bitmap = BitmapFactory.decodeFile(filePath);
-            if (bitmap != null) {
-                // 设置到ImageView中
-                imageView.setImageBitmap(bitmap);
-            }
+        // 动态加载 FlyFragment
+        if (savedInstanceState == null) {  // 确保 Fragment 只会添加一次
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new WayPointV3Fragment()) // 假设有一个容器 view 来放置 Fragment
+                    .commit();
         }
 
     }
